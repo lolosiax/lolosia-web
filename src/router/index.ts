34df93a9e67577a305eb053ec4a29bb5de39bfa4
一七年夏 <2098833867@qq.com>
2023-03-03 10:34:59 +0800

    移动系统菜单路径

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,7 +29,7 @@ export const constantRoutes: RouterTypes = [
         name: 'Dashboard',
         component: () => import('@/views/dashboard/index.vue'),
         //using el svg icon, the elSvgIcon first when at the same time using elSvgIcon and icon
-        meta: { title: 'Dashboard', elSvgIcon: 'Fold', affix: true }
+        meta: { title: 'Dashboard', icon: 'house-door', affix: true }
       }
     ]
   }
