96557fb255278dce5e05af238712cb68847fb0e8
一七年夏 <2098833867@qq.com>
2023-08-02 09:43:03 +0800

    环境变量与修复debugger一个键

--- src/store/debuger.ts
+++ src/store/debuger.ts
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia'
 export const useDebuggerStore = defineStore('debugger', {
   state() {
     return {
+      enable: import.meta.env.VITE_APP_ENV !== 'prod',
       ui: {
         showLayoutGrid: false
       }
--- typings/env.d.ts
+++ typings/env.d.ts
@@ -1,8 +1,9 @@
 declare global {
   interface ImportMetaEnv {
+    readonly VITE_APP_BASE_MODE: 'local' | 'url' | 'port'
     readonly VITE_APP_BASE_URL: string
-    readonly VITE_APP_IMAGE_URL: string
-    readonly VITE_APP_ENV: string
+    readonly VITE_APP_BASE_PORT: number
+    readonly VITE_APP_ENV: 'prod' | 'test' | 'dev'
     // 更多环境变量...
   }
   interface ImportMeta {
