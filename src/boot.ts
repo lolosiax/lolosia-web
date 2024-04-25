08b9db885455ffaacdbaa4d650150d3a56edbbcf
洛洛希雅 <lolosiax@qq.com>
2024-04-25 08:41:17 +0800

    修正一个导入方式

---
+++ src/boot.ts
@@ -0,0 +1,23 @@
+//@ts-ignore
+import css from '@/styles/init.css?url'
+//@ts-ignore
+import logo from '@/icons/common/sidebar-logo.svg?url'
+import settings from '@/settings'
+
+document.querySelector('#app')!.innerHTML = `
+<div class="__init__">
+  <link id="__init__" rel="stylesheet" href="${css}" />
+  <div class="__init__image">
+    <img src="${logo}" alt="" />
+  </div>
+  <div class="__init__title">${settings.title}</div>
+  <div class="__init__process-frame">
+    <div class="__init__progress">
+      <div class="__init__masker"></div>
+    </div>
+  </div>
+</div>
+`
+;(async () => {
+  await import('./main')
+})()
